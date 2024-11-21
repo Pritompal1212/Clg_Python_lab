39d860c3f066eaad87829880d17597b6423d868f
Pritompal1212 <mspritom111@gmail.com>
2024-11-21 10:18:58 +0530

    5th

---
+++ add_&mul_complexnum3-1.py
@@ -0,0 +1,18 @@
+#write a python program to add and multiplycomplex number
+#write a python program to print multiplication table of a given number
+
+# Define two complex numbers
+complex1 = complex(2, 3)  # 2 + 3i
+complex2 = complex(4, 5)  # 4 + 5i
+
+# Addition of complex numbers
+addition = complex1 + complex2
+
+# Multiplication of complex numbers
+multiplication = complex1 * complex2
+
+# Display results
+print("Complex Number 1:", complex1)
+print("Complex Number 2:", complex2)
+print("Addition:", addition)
+print("Multiplication:", multiplication)
