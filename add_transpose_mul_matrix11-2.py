1f9aa6ece74389e05c897acc3241bb4e399c770e
Pritompal1212 <mspritom111@gmail.com>
2024-12-03 22:20:58 +0530

    22th

--- add_transpose_mul_matrix11-2.py
+++ add_transpose_mul_matrix11-2.py
@@ -1,16 +1,15 @@
 def matrix_addition(matrix1, matrix2):
-    result = [[matrix1[i][j] + matrix2[i][j] for j in range(len(matrix1[0]))] for i in range(len(matrix1))]#list comprehesive booltai hai isko
+    result = [[matrix1[i][j] + matrix2[i][j] for j in range(len(matrix1[0]))] for i in range(len(matrix1))]
     return result
 
 def matrix_transpose(matrix):
-    result = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]#ye bhi list comprehensive
+    result = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
     return result
 
 def matrix_multiplication(matrix1, matrix2):
-    result = [[sum(matrix1[i][k] * matrix2[k][j] for k in range(len(matrix2))) for j in range(len(matrix2[0]))] for i in range(len(matrix1))]#list comprehensive meaning list k andhar hi loop start karna samjha bangladeshi
+    result = [[sum(matrix1[i][k] * matrix2[k][j] for k in range(len(matrix2))) for j in range(len(matrix2[0]))] for i in range(len(matrix1))]
     return result
 
-# Input matrices
 matrix1 = [
     [1, 2, 3],
     [4, 5, 6],
@@ -23,19 +22,16 @@ matrix2 = [
     [3, 2, 1]
 ]
 
-# Matrix Addition
 print("\nAddition of Matrices:")
 addition_result = matrix_addition(matrix1, matrix2)
 for row in addition_result:
     print(row)
 
-# Matrix Transpose
 print("\nTranspose of Matrix 1:")
 transpose_result = matrix_transpose(matrix1)
 for row in transpose_result:
     print(row)
 
-# Matrix Multiplication
 print("\nMultiplication of Matrices:")
 multiplication_result = matrix_multiplication(matrix1, matrix2)
 for row in multiplication_result:
